6448f5970d8bcf9d854e39d21c853e41406d6750
Christopher Moore <chris@nuwavecommerce.com>
2018-01-11 11:15:33 -0700

    updated post/user resolvers

--- src/modules/post/post.resolver.ts
+++ src/modules/post/post.resolver.ts
@@ -1,10 +1,43 @@
-import { Resolver, Query } from "@nestjs/graphql";
-import { Context } from "../../utils";
+import { Resolver, Query, Mutation } from "@nestjs/graphql";
+import { Post } from "../../generated/graphcool";
+import { Context, getUserId } from "../../utils";
+
+interface IWritePostArgs {
+  title: string;
+  text: string;
+}
 
 @Resolver("Post")
 export class PostResolver {
   @Query()
-  public async feed(parent: any, args: any, ctx: Context, info: any): Promise<any> {
+  public async feed(_: {}, args: {}, ctx: Context, info: any): Promise<any> {
     return ctx.db.query.posts({ where: { isPublished: true } }, info);
+  }
+
+  @Mutation()
+  public async writePost(_: {}, { title, text }: IWritePostArgs, ctx: Context, info: any): Promise<Post> {
+    const authorId = getUserId(ctx);
+    return ctx.db.mutation.createPost({
+      data: {
+        isPublished: true,
+        title,
+        text,
+        author: {
+          connect: { id: authorId }
+        }
+      }
+    }, info);
+  }
+
+  @Mutation()
+  public async deletePost(_: {}, { id }: { id: string }, ctx: Context, info: any): Promise<Post> {
+    const authorId = getUserId(ctx);
+    const postExists = await ctx.db.exists.Post({ id, author: { id: authorId } });
+
+    if (!postExists) {
+      throw new Error(`Post not found or you're not the author`);
+    }
+
+    return ctx.db.mutation.deletePost({ where: { id } });
   }
 }
--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -1,13 +1,5 @@
-import * as bcrypt from 'bcryptjs'
-import * as jwt from 'jsonwebtoken'
-import { Resolver, Query, Mutation } from "@nestjs/graphql";
+import { Resolver, Query } from "@nestjs/graphql";
 import { Context, getUserId } from "../../utils";
-
-interface SignupMutationArgs {
-  email: string;
-  password: string;
-  name: string;
-}
 
 @Resolver("User")
 export class UserResolver {
@@ -15,20 +7,5 @@ export class UserResolver {
   public async me(_, args: any, context: Context, info: any): Promise<any> {
     const id = getUserId(context);
     return context.db.query.user({ where: { id }  }, info);
-  }
-
-  @Mutation()
-  public async signup(_, args: SignupMutationArgs, context: Context, info: any): Promise<any> {
-    const password = await bcrypt.hash(args.password, 10);
-    console.log({ password });
-    const user = await context.db.mutation.createUser({
-      data: { ...args, password }
-    });
-    console.log({ user });
-
-    return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
-      user
-    };
   }
 }
