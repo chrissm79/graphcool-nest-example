ab9e104fdd14dacd447efee57a23819a7b222c34
Christopher Moore <chris@nuwavecommerce.com>
2018-01-11 15:02:36 -0700

    include apollo-engine

--- src/modules/graphcool/graphcool.module.ts
+++ src/modules/graphcool/graphcool.module.ts
@@ -6,6 +6,7 @@ import {
   RequestMethod
 } from "@nestjs/common";
 import { graphqlExpress } from "apollo-server-express";
+import { Engine } from "apollo-engine";
 import { importSchema } from "graphql-import";
 import { GraphQLModule, GraphQLFactory } from "@nestjs/graphql";
 import { Graphcool } from "../../generated/graphcool";
@@ -19,17 +20,21 @@ import { graphcoolProviders } from "./graphcool.providers";
 export class GraphcoolModule implements NestModule {
   constructor(
     @Inject("Graphcool") private readonly db: Graphcool,
+    @Inject("ApolloEngine") private readonly engine: Engine,
     private readonly graphQLFactory: GraphQLFactory
   ) {}
 
   configure(consumer: MiddlewaresConsumer) {
     const typeDefs = importSchema("./src/schema.graphql");
     const schema = this.graphQLFactory.createSchema({ typeDefs });
+    const useEngine = !!this.engine.expressMiddleware;
 
     consumer
       .apply(
         graphqlExpress(req => ({
           schema,
+          tracing: useEngine,
+          cacheControl: useEngine,
           rootValue: req,
           context: {
             request: req,
@@ -38,5 +43,11 @@ export class GraphcoolModule implements NestModule {
         }))
       )
       .forRoutes({ path: "/graphql", method: RequestMethod.ALL });
+
+    if (useEngine) {
+      consumer
+        .apply(this.engine.expressMiddleware())
+        .forRoutes({ path: "/graphql", method: RequestMethod.ALL });
+    }
   }
 }
--- src/modules/graphcool/graphcool.providers.ts
+++ src/modules/graphcool/graphcool.providers.ts
@@ -1,13 +1,34 @@
-import { Graphcool } from '../../generated/graphcool';
+import { Engine } from "apollo-engine";
+import { Graphcool } from "../../generated/graphcool";
 
 export const graphcoolProviders = [
   {
-    provide: 'Graphcool',
+    provide: "Graphcool",
     useFactory: () => {
       return new Graphcool({
         endpoint: process.env.GRAPHCOOL_ENDPOINT,
         secret: process.env.GRAPHCOOL_SECRET
       });
+    }
+  },
+  {
+    provide: "ApolloEngine",
+    useFactory: async () => {
+      const apiKey = process.env.ENGINE_API_KEY || null;
+
+      if (apiKey) {
+        const engine = new Engine({
+          engineConfig: {
+            apiKey
+          },
+          dumpTraffic: true
+        });
+
+        await engine.start();
+        return engine;
+      } else {
+        return {};
+      }
     }
   }
 ];
