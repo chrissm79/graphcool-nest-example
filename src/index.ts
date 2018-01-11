ddb99e7436ff43837ad8e485aa0e332be9bef3bb
Christopher Moore <chris@nuwavecommerce.com>
2018-01-11 15:02:01 -0700

    remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,5 @@
-// import { GraphQLServer } from 'graphql-yoga'
-// import { Graphcool } from './generated/graphcool'
-// import resolvers from './resolvers'
-//
-// const server = new GraphQLServer({
-//   typeDefs: './src/schema.graphql',
-//   resolvers,
-//   context: req => ({
-//     ...req,
-//     db: new Graphcool({
-//       endpoint: process.env.GRAPHCOOL_ENDPOINT,
-//       secret: process.env.GRAPHCOOL_SECRET,
-//     }),
-//   }),
-// })
-//
-// server.start(({ port }) => console.log(`Server is running on http://localhost:${port}`))
-
-import { NestFactory } from '@nestjs/core';
-import { ApplicationModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ApplicationModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
